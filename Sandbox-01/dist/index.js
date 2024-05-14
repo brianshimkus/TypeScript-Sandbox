fa60caa0dbf3ccd2fc9445606cd5324138a69e6c
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-14 16:56:38 -0500

    Script update

--- Sandbox-01/dist/index.js
+++ Sandbox-01/dist/index.js
@@ -1,3 +1,16 @@
 "use strict";
+// Basic Types
 let id = 5;
-console.log('ID:', id);
+let company = 'Brian Company';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Brian', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brian'],
+    [2, 'Sara'],
+];
