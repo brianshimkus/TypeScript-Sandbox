729200e18a1ce10e7cf7b33b94b52643325aa879
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-14 16:49:45 -0500

    Arrays and tuples

--- Sandbox-01/src/index.ts
+++ Sandbox-01/src/index.ts
@@ -1,3 +1,18 @@
+// Basic Types
 let id: number = 5
+let company: string = 'Brian Company'
+let isPublished: boolean = true
+let x: any = 'Hello'
 
-console.log('ID:', id)
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [1, true, 'Hello']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Brian', true]
+
+// Tuple Array
+let employee: [number, string][]
+employee = [
+	[1, 'Brian'],
+	[2, 'Sara'],
+]
