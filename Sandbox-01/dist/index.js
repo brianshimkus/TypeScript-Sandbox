758e7b78fb5ea60c79d20dea839aa9a7263d582c
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-15 23:33:32 -0500

    Type assertion

--- Sandbox-01/dist/index.js
+++ Sandbox-01/dist/index.js
@@ -36,3 +36,7 @@ const user = {
     id: 1,
     name: 'John',
 };
+// Type Assertion
+let cid = 1;
+// let customerId = <number>cid
+let customerId = cid;
--- Sandbox-01/src/index.ts
+++ Sandbox-01/src/index.ts
@@ -46,3 +46,8 @@ const user = {
 	id: 1,
 	name: 'John',
 }
+
+// Type Assertion
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
