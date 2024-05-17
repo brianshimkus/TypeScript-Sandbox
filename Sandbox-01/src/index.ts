d5a9869d4e2066a06d52f47d3a27fd17809912a6
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-16 23:47:01 -0500

    Function interface

--- Sandbox-01/src/index.ts
+++ Sandbox-01/src/index.ts
@@ -73,3 +73,10 @@ const user1: UserInterface = {
 	id: 1,
 	name: 'John',
 }
+
+interface MathFunc {
+	(x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y: number): number => x + y
+const sub: MathFunc = (x: number, y: number): number => x - y
