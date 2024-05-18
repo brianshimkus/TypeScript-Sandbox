4094de8964c9e31195022ef36db11563ca6a9adb
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-18 00:00:22 -0500

    Button props

--- Sandbox-02/app/page.tsx
+++ Sandbox-02/app/page.tsx
@@ -1,5 +1,9 @@
 import Button from '@/components/button'
 
 export default function Home() {
-	return <Button />
+	return (
+		<main className='min-h-screen flex justify-center items-center'>
+			<Button backgroundColor='bg-red-400' fontSize={80} pillShape={true} />
+		</main>
+	)
 }
--- Sandbox-02/components/button.tsx
+++ Sandbox-02/components/button.tsx
@@ -1,3 +1,11 @@
-export default function Button() {
+export default function Button({
+	backgroundColor,
+	fontSize,
+	pillShape,
+}: {
+	backgroundColor: string
+	fontSize: number
+	pillShape: boolean
+}) {
 	return <button className='bg-blue-500 rounded-md px-4 py-2'>Click Me</button>
 }
