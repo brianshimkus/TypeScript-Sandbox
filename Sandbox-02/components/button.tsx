7ee99baaa855ae2f6a5313db8408f5b02381a304
Brian Shimkus <brianshimkus1@gmail.com>
2024-05-18 00:02:37 -0500

    Extracting type

--- Sandbox-02/components/button.tsx
+++ Sandbox-02/components/button.tsx
@@ -1,11 +1,13 @@
+type Buttonprops = {
+	backgroundColor: string
+	fontSize: number
+	pillShape: boolean
+}
+
 export default function Button({
 	backgroundColor,
 	fontSize,
 	pillShape,
-}: {
-	backgroundColor: string
-	fontSize: number
-	pillShape: boolean
-}) {
+}: Buttonprops) {
 	return <button className='bg-blue-500 rounded-md px-4 py-2'>Click Me</button>
 }
